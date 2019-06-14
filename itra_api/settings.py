3c504c23fcda7d65d680ca908ad0087c57ad87dc
Jakob Roberts <jakobroberts@kpmg.ca>
2019-06-14 12:20:14 -0700

    Enable CORS

--- itra_api/settings.py
+++ itra_api/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_WHITELIST = (
     # TODO - set this properly for production
