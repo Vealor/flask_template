b85198e2d764f6407b5928027b2f2b6adaf0a335
Jakob Roberts <jakobroberts@kpmg.ca>
2019-06-20 16:46:00 -0700

    Fix startup script

--- app.py
+++ app.py
@@ -5,13 +5,13 @@ from src.util import *
 if __name__ == "__main__":
     if len(sys.argv) > 1:
         if sys.argv[1].lower() == "dev":
-            if sys.argv[2].lower() == "ssl":
+            if len(sys.argv) > 2 and sys.argv[2].lower() == "ssl":
                 print(bcolours.OKGREEN + "\n %% DEV SSL %% \n"+ bcolours.ENDC)
                 api.run(debug=True,ssl_context='adhoc')
             else:
                 print(bcolours.OKGREEN + "\n %% DEV %% \n"+ bcolours.ENDC)
                 api.run(debug=True)
-    elif if sys.argv[1].lower() == "ssl":
+    elif sys.argv[1].lower() == "ssl":
         print(bcolours.OKBLUE + "\n %% PROD %% \n"+ bcolours.ENDC)
         api.run(ssl_context='adhoc')
     else:
