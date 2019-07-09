b291e7406bf3527d4313225dc666277dbfadf94d
Will Thompson <willthompson@kpmg.ca>
2019-07-09 14:04:56 -0700

    Removing unnecessary functions

--- src/endpoints/train.py
+++ src/endpoints/train.py
@@ -19,24 +19,38 @@ def do_train():
     response = { 'status': '', 'message': '', 'payload': []}
     data = request.get_json()
     if request.method == 'POST':
-        print(data)
         response['data'] = data
-        # If there are fields missing in the input...
-        if not is_input_complete(data):
+
+        # Check 1: Check if fields are missing in the input.
+        required_keys = input_config.keys()
+        is_input_complete = all([rk in data.keys() for rk in required_keys])
+        if not is_input_complete:
             response['status'] = "error"
             response['message'] = 'Critical field is not specifed'
             return jsonify(response), 400
-        # If the input is not specified correctly...
-        if not is_input_correct(data):
+
+        # Check 2: Check if the input has the correct datatypes.
+        dtypes = [input_config[key] for key in data.keys()]
+        is_input_correct = all([isinstance(key,eval(typ)) for (key,typ) in zip(data.values(),dtypes)])
+        if not is_input_correct:
             response['status'] = "error"
             response['message'] = 'Input field is incorrect type'
             return jsonify(response), 400
-        # Get the date objects. If fail, return an error:
+
+        # Check 3: Get the date objects. If fail, return an error:
         dates = list(data.keys())
         dates.remove("IS_CLIENT_MODEL")
-        #print(dates)
         try:
-            check_and_get_date_configuration(data)
+            train_start = get_date_obj_from_str(data["TRAIN_DATA_START"])
+            train_end = get_date_obj_from_str(data["TRAIN_DATA_END"])
+            if train_start >= train_end:
+                raise ValueError("Invalid Train Data date range.")
+            test_start = get_date_obj_from_str(data["TEST_DATA_START"])
+            test_end = get_date_obj_from_str(data["TEST_DATA_END"])
+            if test_start >= test_end:
+                raise ValueError("Invalid Test Data date range.")
+            if ((train_start < test_start) and (test_start < train_end)) or ((test_start < train_start) and not (test_end < train_start)):
+                raise ValueError("Train and test data ranges overlap.")
         except ValueError as e:
             response['status'] = "error"
             response['message'] = str(e)
@@ -46,17 +60,8 @@ def do_train():
 
     return jsonify(response), 202
 
-# Check that all fields are added to input
-def is_input_complete(data):
-    required_keys = input_config.keys()
-    return all([rk in data.keys() for rk in required_keys])
 
-# RETURNS: bool
-# Check that input is the correct type.
-def is_input_correct(data):
-    dtypes = [input_config[key] for key in data.keys()]
-    return all([isinstance(key,eval(typ)) for (key,typ) in zip(data.values(),dtypes)])
-
+## ===== HELPERS ================================ ##
 # RETURNS: datetime.datetime.date obj
 # Dates should be specified as "YYYY-MM-DD"
 def get_date_obj_from_str(date_str):
@@ -65,17 +70,3 @@ def get_date_obj_from_str(date_str):
     except ValueError:
         raise ValueError("Incorrect data format, should be YYYY-MM-DD")
     return date_obj
-
-# Check if dates are correctly specified so that no overlap occurs
-def check_and_get_date_configuration(data):
-    train_start = get_date_obj_from_str(data["TRAIN_DATA_START"])
-    train_end = get_date_obj_from_str(data["TRAIN_DATA_END"])
-    if train_start >= train_end:
-        raise ValueError("Invalid Train Data date range.")
-    test_start = get_date_obj_from_str(data["TEST_DATA_START"])
-    test_end = get_date_obj_from_str(data["TEST_DATA_END"])
-    if test_start >= test_end:
-        raise ValueError("Invalid Test Data date range.")
-    if ((train_start < test_start) and (test_start < train_end)) or ((test_start < train_start) and not (test_end < train_start)):
-        raise ValueError("Train and test data ranges overlap.")
-    return train_start, train_end, test_start, test_end
