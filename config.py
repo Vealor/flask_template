877826fabd470e074d5eade041c769de95f9017e
Jakob Roberts <jakobroberts@kpmg.ca>
2019-07-16 15:44:47 -0700

    Bump to 0.1.4

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 from datetime import timedelta
 
 class Config(object):
-    VERSION = '0.1.3'
+    VERSION = '0.1.4'
 
     # Application threads. A common general assumption is
     # using 2 per available processor cores - to handle
