ec4cc7a3f40be24a1677ae1d16309b1be9588c6c
jonathanho1 <jonathanho1@kpmg.ca>
2019-07-26 14:52:07 -0700

    Add sap_caps_gen to 55

--- src/endpoints/sap_caps_gen.py
+++ src/endpoints/sap_caps_gen.py
@@ -176,7 +176,7 @@ def build_master_tables():
         db.session.bulk_insert_mappings(referenceclass, exampledump)
         db.session.commit()
         print('great success')
-    return 'OK' 
+    return 'OK'
 
 @sap_caps_gen.route('/data_quality_check', methods=['GET'])
 def data_quality_check():
