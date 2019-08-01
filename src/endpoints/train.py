236c4d72d89110c434dc2071d6b8f12871e60b5a
Will Thompson <willthompson@kpmg.ca>
2019-08-01 08:21:50 -0700

    Resolving differences with train.py

--- src/endpoints/train.py
+++ src/endpoints/train.py
@@ -4,7 +4,6 @@ Train Endpoints
 import datetime
 import json
 import random
-from config import DevelopmentConfig
 from flask import Blueprint, current_app, jsonify, request
 from flask_jwt_extended import jwt_required
 from src.models import *
@@ -28,7 +27,7 @@ def do_train():
     if request.method == 'POST':
         request_types = {
             'MODEL_TYPE': 'str',
-            'CLIENT_ID': 'int',     #If MODEL_TYPE == 'master', assert equal to -1
+            'CLIENT_ID': 'int',
             'TRAIN_DATA_START_DATE': 'str',
             'TRAIN_DATA_END_DATE': 'str',
             'TEST_DATA_START_DATE': 'str',
@@ -54,13 +53,30 @@ def do_train():
             if not (train_end < test_start or test_end < train_start):
                 raise ValueError('Train and test data ranges overlap.')
 
-        except ValueError as e:
+            # At this point, all database independent checks have been perfrm'd
+            # Now, database dependent checks begin
+            if data['MODEL_TYPE'] == 'client':
+                # Is the client id in the DATABASE?
+                if not Client.find_by_id(data['CLIENT_ID']):
+                    raise ValueError('Client id \'{}\' is not in the database.'.format(data['CLIENT_ID']))
+                # Check if any projects exist for the client
+                client_projects = [p.id for p in Project.query.filter_by(client_id = data['CLIENT_ID']).distinct()]
+                if len(client_projects) == 0:
+                    raise ValueError('Client id \'{}\' has no projects in the database.'.format(data['CLIENT_ID']))
+                # Get transactions
+                transactions_count = Transaction.query.filter(Transaction.project_id.in_(client_projects)).count()
+                if transactions_count < 6000:
+                    raise Exception('Not enough data to train a client model.')
+            else:
+                if Transaction.query.count() < 10000:
+                    raise Exception('Not enough data to train a master model.')
+
+
+        except Exception as e:
             response['status'] = 'error'
             response['message'] = str(e)
             return jsonify(response), 400
 
-        # At this point, all database independent checks have been perfrm'd
-        # Now, database dependent checks begin (TO BE COMPLETED LATER)
 
         # Now that all the database checks have been completed, we can submit
         # our request to the compute server
