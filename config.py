18eb82385c7b2dae378223b309faf2a6c44aeaac
Will Thompson <willthompson@kpmg.ca>
2019-08-12 13:24:59 -0700

    Incre. version number

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 from datetime import timedelta
 
 class Config(object):
-    VERSION = '0.1.14'
+    VERSION = '0.1.15'
 
     # Application threads. A common general assumption is
     # using 2 per available processor cores - to handle
