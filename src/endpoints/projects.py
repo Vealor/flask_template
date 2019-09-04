26ae73155cfb3cc881ef737b8f260b334dfa489d
Cio Ellorin <cellorin@kpmg.ca>
2019-09-04 13:34:35 -0700

    Add current user to projects

--- src/endpoints/projects.py
+++ src/endpoints/projects.py
@@ -4,7 +4,7 @@ Project Endpoints
 import json
 import random
 from flask import Blueprint, current_app, jsonify, request
-from flask_jwt_extended import (jwt_required, jwt_refresh_token_required, get_jwt_identity, get_raw_jwt)
+from flask_jwt_extended import (jwt_required, jwt_refresh_token_required, get_jwt_identity, get_raw_jwt, current_user)
 from src.models import *
 from src.util import validate_request_data
 
