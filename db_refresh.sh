6d9ebba47aa8d1440a5765015a029980d7ae65f0
Jakob Roberts <jakobroberts@kpmg.ca>
2019-09-13 16:20:36 -0700

    Fix DB update

--- db_refresh.sh
+++ db_refresh.sh
@@ -11,11 +11,39 @@ EOF
 source activate
 FLASK_ENV='development' flask db upgrade
 
-read -n 1 -s -r -p "DB Entry Next >> Press any key to continue" && printf "\n"
+read -n 1 -s -r -p "START SERVER NOW >> Press any key to continue once started" && printf "\n"
+
+function make_user () {
+  curl -H "Content-Type: application/json" -X POST -d '{
+    "username":"'$1'",
+    "password":"'$2'",
+    "email":"'$3'",
+    "initials":"'$4'",
+    "first_name":"'$5'",
+    "last_name":"'$6'",
+    "role":"'$7'"
+  }' http://localhost:5000/users
+}
+make_user "test" "test" "lh_test_user@test.test" "TEST" "test_first" "test_last" "tax_master"
+
+make_user "pepperpotts" "test" "pepperpotts@test.test" "pp" "pepper" "potts" "tax_practitioner"
+make_user "antman" "test" "antman@test.test" "am" "ant" "man" "tax_practitioner"
+make_user "spiderman" "test" "spiderman@test.test" "sm" "spider" "man" "tax_practitioner"
+make_user "ironman" "test" "ironman@test.test" "im" "iron" "man" "tax_approver"
+make_user "blackpanther" "test" "blackpanther@test.test" "bp" "black" "panther" "tax_approver"
+make_user "deadpool" "test" "deadpool@test.test" "dp" "dead" "pool" "tax_approver"
+make_user "captainamerica" "test" "captainamerica@test.test" "ca" "captain" "america" "tax_master"
+make_user "captainmarvel" "test" "captainmarvel@test.test" "cm" "captain" "marvel" "tax_master"
+make_user "lukecage" "test" "lukecage@test.test" "lc" "luke" "cage" "tax_master"
+make_user "incrediblehulk" "test" "incrediblehulk@test.test" "ih" "incredible" "hulk" "data_master"
+make_user "doctorstrange" "test" "doctorstrange@test.test" "drs" "doctor" "strange" "data_master"
+make_user "hawkeye" "test" "hawkeye@test.test" "hwk" "hawk" "eye" "data_master"
+make_user "blackwidow" "test" "blackwidow@test.test" "bw" "black" "widow" "administrative_assistant"
+make_user "edwinjarvis" "test" "edwinjarvis@test.test" "ej" "edwin" "jarvis" "administrative_assistant"
+make_user "philcoulson" "test" "philcoulson@test.test" "pc" "phil" "coulson" "administrative_assistant"
+make_user "nickfury" "test" "nickfury@test.test" "nf" "nick" "fury" "tax_practitioner"
 
 psql -h localhost -U itra itra_db -c "
-  insert into users (role, username, password, email, initials, first_name, last_name, is_superuser)
-    values ('tax_master', 'test', 'test', 'lh_test_user@kpmg.ca', 'TEST', 'test_first', 'test_last', 't');
   insert into logs (action, affected_entity, details, user_id) values('create', 'everything', 'such detail', 1);
 
   insert into clients (name) values ('mining corp');
@@ -34,39 +62,10 @@ psql -h localhost -U itra itra_db -c "
   insert into transactions (data, vendor_id, project_id) values ('{}', 1, 1);
   insert into user_project (user_id, project_id) values (1, 1);
 
-  insert into users (role, username, password, email, initials, first_name, last_name)
-    values ('tax_practitioner', 'pepperpotts', 'test', 'pepperpotts@test.test', 'pp', 'pepper', 'potts');
-  insert into users (role, username, password, email, initials, first_name, last_name)
-    values ('tax_practitioner', 'antman', 'test', 'antman@test.test', 'am', 'ant', 'man');
-  insert into users (role, username, password, email, initials, first_name, last_name)
-    values ('tax_practitioner', 'spiderman', 'test', 'spiderman@test.test', 'sm', 'spider', 'man');
-  insert into users (role, username, password, email, initials, first_name, last_name)
-    values ('tax_approver', 'ironman', 'test', 'ironman@test.test', 'im', 'iron', 'man');
-  insert into users (role, username, password, email, initials, first_name, last_name)
-    values ('tax_approver', 'blackpanther', 'test', 'blackpanther@test.test', 'bp', 'black', 'panther');
-  insert into users (role, username, password, email, initials, first_name, last_name)
-    values ('tax_approver', 'deadpool', 'test', 'deadpool@test.test', 'dp', 'dead', 'pool');
-  insert into users (role, username, password, email, initials, first_name, last_name)
-    values ('tax_master', 'captainamerica', 'test', 'captainamerica@test.test', 'ca', 'captain', 'america');
-  insert into users (role, username, password, email, initials, first_name, last_name)
-    values ('tax_master', 'captainmarvel', 'test', 'captainmarvel@test.test', 'cm', 'captain', 'marvel');
-  insert into users (role, username, password, email, initials, first_name, last_name)
-    values ('tax_master', 'lukecage', 'test', 'lukecage@test.test', 'lc', 'luke', 'cage');
-  insert into users (role, username, password, email, initials, first_name, last_name)
-    values ('data_master', 'incrediblehulk', 'test', 'incrediblehulk@test.test', 'ih', 'incredible', 'hulk');
-  insert into users (role, username, password, email, initials, first_name, last_name)
-    values ('data_master', 'doctorstrange', 'test', 'doctorstrange@test.test', 'drs', 'doctor', 'strange');
-  insert into users (role, username, password, email, initials, first_name, last_name)
-    values ('data_master', 'hawkeye', 'test', 'hawkeye@test.test', 'hwk', 'hawk', 'eye');
-  insert into users (role, username, password, email, initials, first_name, last_name)
-    values ('administrative_assistant', 'blackwidow', 'test', 'blackwidow@test.test', 'bw', 'black', 'widow');
-  insert into users (role, username, password, email, initials, first_name, last_name)
-    values ('administrative_assistant', 'edwinjarvis', 'test', 'edwinjarvis@test.test', 'ej', 'edwin', 'jarvis');
-  insert into users (role, username, password, email, initials, first_name, last_name)
-    values ('administrative_assistant', 'philcoulson', 'test', 'philcoulson@test.test', 'pc', 'phil', 'coulson');
-  insert into users (role, username, password, email, initials, first_name, last_name, is_system_administrator)
-    values ('tax_practitioner', 'nickfury', 'test', 'nickfury@test.test', 'nf', 'nick', 'fury', 't');
-
+  update users set req_pass_reset = 'f';
+  update users set is_superuser = 'y' where id = 1;
+  update users set is_system_administrator = 'y' where id = 17;
+  
   insert into user_project (user_id, project_id) values (2, 1);
   insert into user_project (user_id, project_id) values (3, 2);
   insert into user_project (user_id, project_id) values (3, 3);
