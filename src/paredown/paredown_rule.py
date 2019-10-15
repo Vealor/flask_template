fec11318a411ef7e6f2f97a420b6729bc7ca5367
Will Thompson <willthompson@kpmg.ca>
2019-10-15 13:43:21 -0700

    Add exception for missing rule

--- src/paredown/paredown_rule.py
+++ src/paredown/paredown_rule.py
@@ -9,5 +9,12 @@ class ParedownRule:
         self.comment = comment
 
     def apply_to_data(self, df):
-        fields_functions = [(pc.field_name,pc.value_type,pc.get_evaluator()) for pc in self.paredown_conditions]
-        return reduce(lambda x,y: x & y, [df[ff[0]].astype(ff[1]).apply(ff[2]) for ff in fields_functions])
+        try:
+            # Check if paredown columns exist
+            if not all([col in df.columns for col in [pdc.field_name for pdc in self.paredown_conditions]]):
+                raise ValueError("Cannot apply rule. DataFrame does not contain at least one required field_name.")
+
+            fields_functions = [(pc.field_name,pc.value_type,pc.get_evaluator()) for pc in self.paredown_conditions]
+            return reduce(lambda x,y: x & y, [df[ff[0]].astype(ff[1]).apply(ff[2]) for ff in fields_functions])
+        except Exception as e:
+            raise Exception("Applying paredown rule failed: {}".format(str(e)))
