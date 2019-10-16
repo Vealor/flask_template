289ae53be4eb27256a6a1f32fa20056ec94d0c11
Will Thompson <willthompson@kpmg.ca>
2019-10-16 13:55:48 -0700

    Add update functionality

--- src/endpoints/paredown.py
+++ src/endpoints/paredown.py
@@ -28,46 +28,11 @@ def get_paredown_rules():
     except Exception as e:
         response = { 'status': 'error', 'message': str(e), 'payload': [] }
         return jsonify(response), 500
-    return jsonify(response)
-
-# Apply Paredown rules
-@paredown.route('/apply/', methods=['POST'])
-# @jwt_required
-def apply_paredown_rules(df=None):
-    response = { 'status': 'ok', 'message': '', 'payload': [] }
-
-
-    try:
-        # Retrieve and create the rule objects
-        paredown_rule_entries = ParedownRule.query
-        rules = []
-        for pr in paredown_rule_entries:
-            i = pr.id
-            conds = ParedownRuleCondition.query.filter_by(paredown_rule_id=i).all()
-            pcs = [ParedownConditionObject(*(p.field,p.operator,p.value)) for p in conds]
-            rules.append(ParedownRuleObject(pcs, pr.code, pr.comment))
-
-        # Now apply the rules to the data and append the codes as appropriate
-        paredown_codes = {i: set('') for i in range(len(df))}
-        for r in rules:
-            print(r.code)
-            inds = list(df.loc[r.apply_to_data(df)].index)
-            for i in inds:
-                paredown_codes[i] |= set([str(r.code)])
-
-
-        paredown_codes = [','.join(x) for x in paredown_codes.values()]
-        response['payload'] = paredown_codes
-    except ValueError as e:
-        response = { 'status': 'error', 'message': str(e), 'payload': [] }
-        return jsonify(response), 400
-    except Exception as e:
-        response = { 'status': 'error', 'message': str(e), 'payload': [] }
-        return jsonify(response), 500
     return jsonify(response), 200
 
+#===============================================================================
 # Create Paredown rules
-@paredown.route('/create/', methods=['POST'])
+@paredown.route('/', methods=['POST'])
 # @jwt_required
 def create_paredown_rule():
     response = { 'status': 'ok', 'message': '', 'payload': [] }
@@ -126,9 +91,113 @@ def create_paredown_rule():
         db.session.rollback()
         response = { 'status': 'error', 'message': str(e), 'payload': [] }
         return jsonify(response), 500
+    return jsonify(response), 201
+
+#===============================================================================
+# Apply Paredown rules
+@paredown.route('/apply/', methods=['POST'])
+# @jwt_required
+def apply_paredown_rules(df=None):
+    response = { 'status': 'ok', 'message': '', 'payload': [] }
+
+
+    try:
+        # Retrieve and create the rule objects
+        paredown_rule_entries = ParedownRule.query
+        rules = []
+        for pr in paredown_rule_entries:
+            i = pr.id
+            conds = ParedownRuleCondition.query.filter_by(paredown_rule_id=i).all()
+            pcs = [ParedownConditionObject(*(p.field,p.operator,p.value)) for p in conds]
+            rules.append(ParedownRuleObject(pcs, pr.code, pr.comment))
+
+        # Now apply the rules to the data and append the codes as appropriate
+        paredown_codes = {i: set('') for i in range(len(df))}
+        for r in rules:
+            print(r.code)
+            inds = list(df.loc[r.apply_to_data(df)].index)
+            for i in inds:
+                paredown_codes[i] |= set([str(r.code)])
+
+
+        paredown_codes = [','.join(x) for x in paredown_codes.values()]
+        response['payload'] = paredown_codes
+    except ValueError as e:
+        response = { 'status': 'error', 'message': str(e), 'payload': [] }
+        return jsonify(response), 400
+    except Exception as e:
+        response = { 'status': 'error', 'message': str(e), 'payload': [] }
+        return jsonify(response), 500
     return jsonify(response), 200
 
-# Delete Paredown Rule
+#===============================================================================
+# Update a Paredown rule
+@paredown.route('/<path:id>', methods=['PUT'])
+# @jwt_required
+def update_paredown_rule(id):
+    response = { 'status': 'ok', 'message': '', 'payload': [] }
+    data = request.get_json()
+    try:
+        # Validate the fields of the updated paredown rule
+        request_types = {
+            'code': 'str',
+            'comment': 'str',
+            'is_core': 'bool'
+        }
+        validate_request_data(data, request_types)
+
+        if data['code'] == '':
+            raise ValueError("Cannot create paredown rule with no code.")
+
+        # Validate each condition of the new paredown rule
+        if len(data['conditions']) == 0:
+            raise ValueError("Cannot create paredown rule with no conditions.")
+
+        request_types_conditions = {
+            'field': 'str',
+            'operator': 'str'
+        }
+
+        for cond in data['conditions']:
+            validate_request_data(cond, request_types_conditions)
+
+
+        query = ParedownRule.find_by_id(id)
+        if not query:
+            raise ValueError("Pareddown Rule ID {} does not exist.".format(id))
+        query.code = data['code']
+        query.comment = data['comment']
+        query.is_core = data['is_core']
+
+        # Delete and recreate the paredown conditions
+        conditions = ParedownRuleCondition.query.filter_by(paredown_rule_id=id).all()
+        for cond in conditions:
+            db.session.delete(cond)
+
+        new_conditions = data['conditions']
+        for cond in new_conditions:
+            new_paredown_condition = ParedownRuleCondition(
+                field = cond['field'],
+                operator = cond['operator'],
+                value = cond['value'],
+                paredown_rule_id = query.id
+            )
+            db.session.add(new_paredown_condition)
+            db.session.flush()
+
+        db.session.commit()
+        response['message'] = 'Successfully updated Paredown Rule ID {}.'.format(query.id)
+
+    except ValueError as e:
+        response = { 'status': 'error', 'message': str(e), 'payload': [] }
+        return jsonify(response), 400
+    except Exception as e:
+        response = { 'status': 'error', 'message': str(e), 'payload': [] }
+        return jsonify(response), 500
+    return jsonify(response), 200
+
+#===============================================================================
+# DELETE A PAREDOWN RULE
 @paredown.route('/<path:paredown_rule_id>', methods=['DELETE'])
 # @jwt_required
 def delete_paredown_rule(paredown_rule_id):
