1e5af5791fc2f9c4be4eb65cf0b5d70d2bff19f6
Claudia Li <claudiali1@kpmg.ca>
2019-10-21 13:41:20 -0700

    Style tax_rate.py follow PEP8

--- src/endpoints/tax_rate.py
+++ src/endpoints/tax_rate.py
@@ -1,34 +1,26 @@
 '''
 General Endpoints
 '''
-import json
-import random
-from sqlalchemy import exc
-import datetime
-import json
-import requests
-from flask import Blueprint, current_app, jsonify, request
-from src.models import *
-from src.util import *
 from sqlalchemy import desc
-
+from flask import Blueprint, jsonify
+from src.models import Project, CapsGen, SapT007s
 
 tax_rate = Blueprint('tax_rate', __name__)
 #===============================================================================
 # General
-@tax_rate.route('/<int:id>', methods=['GET'])
-def get_tax_rate_table(id):
+@tax_rate.route('/<int:project_id>', methods=['GET'])
+def get_tax_rate_table(project_id):
     try:
         code = 500
         response = {'status': 'ok', 'message': '', 'payload': []}
-        if Project.query.filter_by(id=id).count() < 1:
+        if Project.query.filter_by(id=project_id).count() < 1:
             code = 404
             raise ValueError('Project ID invalid.')
         # check if caps_gen exist, if exist proceed, if not send back error message
-        capsgen = CapsGen.query.filter_by(project_id=id).order_by(desc(CapsGen.id)).first()
+        capsgen = CapsGen.query.filter_by(project_id=project_id).order_by(desc(CapsGen.id)).first()
         if capsgen is not None:
             rows = SapT007s.query.filter_by(capsgen_id=capsgen.id).all()
-            tax_rate_data = [ row.data for row in rows]
+            tax_rate_data = [row.data for row in rows]
             response['payload'] = tax_rate_data
             code = 200
         else:
@@ -39,4 +31,3 @@ def get_tax_rate_table(id):
         response['status'] = 'error'
         response['payload'] = []
     return jsonify(response), code
-
