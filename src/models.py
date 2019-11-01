9ec87a9433dc5232801850f167df1e015914206e
Will Thompson <willthompson@kpmg.ca>
2019-11-01 13:16:53 -0700

    Add model name to returns for Master and Client models.

--- src/models.py
+++ src/models.py
@@ -741,6 +741,7 @@ class ClientModel(db.Model):
             'id': self.id,
             'client_id': self.client_id,
             'created':self.created.strftime("%Y-%m-%d_%H:%M:%S"),
+            'name': self.client_model_client.name + "_" + self.created.strftime("%Y-%m-%d) + "_" + str(self.id),
             'hyper_p': self.hyper_p,
             'status': self.status.value,
             'train_data_start': self.train_data_start.strftime('%Y/%m/%d'),
@@ -796,6 +797,7 @@ class MasterModel(db.Model):
         return {
             'id': self.id,
             'created':self.created.strftime("%Y-%m-%d_%H:%M:%S"),
+            'name': "Master_Model_" + self.created.strftime("%Y-%m-%d) + "_" + str(self.id),
             'hyper_p': self.hyper_p,
             'status': self.status.value,
             'train_data_start': self.train_data_start.strftime('%Y/%m/%d'),
