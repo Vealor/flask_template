08b6b024875cb66936bb91f4237ca0f020e812fa
jonathanho1 <jonathanho1@kpmg.ca>
2019-11-04 16:41:12 -0800

    Update to azure

--- src/util.py
+++ src/util.py
@@ -79,7 +79,7 @@ def send_mail(user_email, subject, content):
 
 #===============================================================================
 # Unzips all SAP source data text files from a nested zip file.
-def source_data_unzipper(data, response):
+def source_data_unzipper(data, response, project_id):
 
     def extract_nested_zip(zippedFile, toFolder, outputPath):
         try:
@@ -114,8 +114,9 @@ def source_data_unzipper(data, response):
             raise Exception(e)
 
     if os.environ['FLASK_ENV'] == 'development':
-        current_input_path = os.path.join(os.getcwd(), current_app.config['CAPS_BASE_DIR'],  str(data['project_id']), current_app.config['CAPS_RAW_LOCATION'])
-        current_output_path = os.path.join(os.getcwd(), current_app.config['CAPS_BASE_DIR'], str(data['project_id']), current_app.config['CAPS_UNZIPPING_LOCATION'])
+        #https://itrauat.file.core.windows.net/caps-gen-processing/6
+        current_input_path = 'https://itrauat.file.core.windows.net/caps-gen-processing/{project_id}/{CAPS_RAW_LOCATION}'.format(project_id = project_id, CAPS_RAW_LOCATION = current_app.config['CAPS_RAW_LOCATION'])
+        current_output_path = 'https://itrauat.file.core.windows.net/caps-gen-processing/{project_id}/{CAPS_UNZIPPING_LOCATION}'.format(project_id = project_id, CAPS_UNZIPPING_LOCATION = current_app.config['CAPS_UNZIPPING_LOCATION'])
         if data['file_name'].lower().endswith('.zip'):
             extract_nested_zip(os.path.join( current_input_path, data['file_name']), current_output_path, current_output_path)
             move_nested_folder(current_output_path, current_output_path)
