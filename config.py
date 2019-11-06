7dbac17a05e139a5e6430e903ea6e04fe7b659ad
Jakob Roberts <jakobroberts@kpmg.ca>
2019-11-06 10:30:44 -0800

    Extend access token for development

--- config.py
+++ config.py
@@ -25,6 +25,8 @@ class Config(object):
 class DevelopmentConfig(Config):
     # Statement for enabling the development environment
     DEBUG = True
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(day=1)
+    JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=6)
 
     # Use a secure, unique and absolutely secret key for signing the data.
     CSRF_SESSION_KEY = "secret"
