db08f60beadb44133116237cebbc66d4b7deacb3
Will Thompson <willthompson@kpmg.ca>
2019-11-06 16:00:06 -0800

    Finish the transactions to df query

--- src/prediction/database.py
+++ src/prediction/database.py
@@ -4,7 +4,8 @@ from src.models import Transaction, Code
 def transactions_to_dataframe(query,**kwargs):
 
     # Get the 'data' and 'code' field for all transactions in query and merge them into dataframe
-    entries, codes = zip(*[(tr.serialize['data'],tr.gst_code) for tr in query])
-    entries = pd.read_json('[' + ','.join(train_entries) + ']',orient='records')
+    entries, codes = zip(*[(tr.serialize['data'],tr.gst_code.code_number) for tr in query])
+    entries = pd.read_json('[' + ','.join(entries) + ']',orient='records')
+    entries['Code'] = codes
 
-    # Get the appropriate codes from the code table
+    return entries
