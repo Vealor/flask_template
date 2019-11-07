59f92c992d5a2bd02e7369321ae150dd85774b17
Jakob Roberts <jakobroberts@kpmg.ca>
2019-11-07 13:14:16 -0800

    Add ITRA users

--- db_scripts/create_users.sh
+++ db_scripts/create_users.sh
@@ -11,6 +11,20 @@ function make_user () {
     "role":"'$7'"
   }' $8:5000/users
 }
+
+function make_system_administrator () {
+  curl -H "Content-Type: application/json" -X POST -d '{
+    "username":"'$1'",
+    "password":"'$2'",
+    "email":"'$3'",
+    "initials":"'$4'",
+    "first_name":"'$5'",
+    "last_name":"'$6'",
+    "role":"'$7'",
+    "is_system_administrator":1
+  }' $8:5000/users
+}
+
 make_user "test" "test" "lh_test_user@test.test" "TEST" "test_first" "test_last" "tax_master" $6
 
 make_user "pepperpotts" "test" "pepperpotts@test.test" "pp" "pepper" "potts" "tax_practitioner" $6
@@ -29,3 +43,15 @@ make_user "blackwidow" "test" "blackwidow@test.test" "bw" "black" "widow" "admin
 make_user "edwinjarvis" "test" "edwinjarvis@test.test" "ej" "edwin" "jarvis" "administrative_assistant" $6
 make_user "philcoulson" "test" "philcoulson@test.test" "pc" "phil" "coulson" "administrative_assistant" $6
 make_user "nickfury" "test" "nickfury@test.test" "nf" "nick" "fury" "tax_practitioner" $6
+
+# LH GVA USER
+# curl -H "Content-Type: application/json" -X POST -d '{}' $8:5000/auth/create_base_lh_superuser
+
+# ITRA Users
+make_system_administrator "ejensen" "ejensen" "ejensen@kpmg.ca" "edj" "Erin" "Jensen" "tax_master" $6
+make_system_administrator "nataliakrizbai" "nataliakrizbai" "nataliakrizbai@kpmg.ca" "nk" "Natalia" "Krizbai" "tax_master" $6
+make_system_administrator "dcallow" "dcallow" "dcallow@kpmg.ca" "dtc" "Dylan" "Callow" "tax_master" $6
+make_system_administrator "ryansen" "ryansen" "ryansen@kpmg.ca" "rjs" "Ryan" "Sen" "tax_master" $6
+make_system_administrator "karinadewi" "karinadewi" "karinadewi@kpmg.ca" "kd" "Karina" "Dewi" "tax_master" $6
+make_system_administrator "jasonclee" "jasonclee" "jasonclee@kpmg.ca" "jcl" "Jason" "Lee" "tax_master" $6
+make_system_administrator "andylee1" "andylee1" "andylee1@kpmg.ca" "al" "Andy" "Lee" "tax_master" $6
