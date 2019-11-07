b38bc2e5474a788e5d277ae816490dd2231a859f
Will Thompson <willthompson@kpmg.ca>
2019-11-06 18:41:30 -0800

    If no tr code has been assigned, assigned it -999

--- src/prediction/database.py
+++ src/prediction/database.py
@@ -4,7 +4,7 @@ from src.models import Transaction, Code
 def transactions_to_dataframe(query,**kwargs):
 
     # Get the 'data' and 'code' field for all transactions in query and merge them into dataframe
-    entries, codes = zip(*[(tr.serialize['data'],tr.gst_code.code_number) for tr in query])
+    entries, codes = zip(*[(tr.serialize['data'],(tr.gst_code.code_number if tr.gst_code else -999)) for tr in query])
     entries = pd.read_json('[' + ','.join(entries) + ']',orient='records')
     entries['Code'] = codes
 
