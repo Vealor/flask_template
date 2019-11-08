13cec4bf4b8b41722e9ac768f42e882e67872dff
Jakob Roberts <jakobroberts@kpmg.ca>
2019-11-08 14:41:17 -0800

    Rebuild init script

--- src/__init__.py
+++ src/__init__.py
@@ -11,22 +11,27 @@ from pytz import timezone
 from src.util import *
 #===============================================================================
 # API Creation & Configuration
-api = Flask(__name__) # global application initialization
-CORS(api, supports_credentials=True)
+def build_api():
+    api = Flask(__name__)
+    CORS(api, supports_credentials=True)
 
-if api.config['ENV'] == 'development':
-    print(bcolours.OKGREEN + "\n %% DEV %% \n"+ bcolours.ENDC)
-    api.config.from_object('config.DevelopmentConfig')
-elif api.config['ENV'] == 'testing':
-    print(bcolours.WARNING + "\n %% TEST %% \n"+ bcolours.ENDC)
-    api.config.from_object('config.TestingConfig')
-elif api.config['ENV'] == 'production':
-    print(bcolours.OKBLUE + "\n %% PROD %% \n"+ bcolours.ENDC)
-    api.config.from_object('config.ProductionConfig')
-else:
-    raise RuntimeError('CONFIGURATION STARTUP ERROR')
+    if api.config['ENV'] == 'development':
+        print(bcolours.OKGREEN + "\n %% DEV %% \n"+ bcolours.ENDC)
+        api.config.from_object('config.DevelopmentConfig')
+    elif api.config['ENV'] == 'testing':
+        print(bcolours.WARNING + "\n %% TEST %% \n"+ bcolours.ENDC)
+        api.config.from_object('config.TestingConfig')
+    elif api.config['ENV'] == 'production':
+        print(bcolours.OKBLUE + "\n %% PROD %% \n"+ bcolours.ENDC)
+        api.config.from_object('config.ProductionConfig')
+    else:
+        raise RuntimeError('CONFIGURATION STARTUP ERROR')
 
-api.url_map.strict_slashes = False
+    api.url_map.strict_slashes = False
+    return api
+
+api = build_api()
+
 db.init_app(api)
 migrate = Migrate(api, db)
 jwt = JWTManager(api)
