cfda009a0207422c611b6234e00da0ca94ad2dff
Will Thompson <willthompson@kpmg.ca>
2019-11-08 14:07:28 -0800

    Covert python script to bash script for model insertion

--- db_refresh.sh
+++ db_refresh.sh
@@ -35,4 +35,4 @@ sleep 1 && echo -e "$FAKE\tInserting Nexen data mappings - EMPTY CAPSGEN MAPPING
 ./db_scripts/_insert_nexen_data_mappings_mk2.sh
 
 sleep 1 && echo -e "$FAKE\tInserting dummy prediction models" && sleep 1
-python _insert_dummy_prediction_models.py
+./db_scripts/_insert_dummy_prediction_models.sh
