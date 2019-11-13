00ebcd27bcee58a738dedf2dd1ebacd18648913a
Jakob Roberts <jakobroberts@kpmg.ca>
2019-11-13 15:30:56 -0800

    Add custom args for cdm_labels for caps_table

--- src/endpoints/cdm_labels.py
+++ src/endpoints/cdm_labels.py
@@ -23,11 +23,14 @@ def get_cdm_labels():
     query = CDMLabel.query
     # Set ORDER
     query = query.order_by('script_label')
-    # Set LIMIT
-    query = query.limit(args['limit']) if 'limit' in args.keys() and args['limit'].isdigit() else query.limit(1000)
-    # Set OFFSET
-    query = query.offset(args['offset']) if 'offset' in args.keys() and args['offset'].isdigit() else query.offset(0)
 
-    response['payload'] = [i.serialize for i in query.all()]
+    # Set LIMIT
+    if 'caps_table' in args.keys():
+        output = {}
+        for i in query.all():
+            output[i.script_label]={'display_name':i.display_name,'caps_interface':i.caps_interface.value if i.caps_interface else None}
+        response['payload'] = output
+    else:
+        response['payload'] = [i.serialize for i in query.all()]
 
     return jsonify(response)
