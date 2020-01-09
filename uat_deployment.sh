7e68e3033f7dc9651bfd3346c53b4195fb96033d
jonathanho1 <jonathanho1@kpmg.ca>
2020-01-08 17:04:44 -0800

    Fix automated deployment

--- uat_deployment.sh
+++ uat_deployment.sh
@@ -1,16 +1,8 @@
 USERNAME=lh-admin-tax
 PASSWORD=Kpmg1234$
-sleep 1 && echo -e "\tChecking if Azure remote URL exists" && sleep 1
-AZURE_EXISTS=$(git remote -v | grep azure)
 
-if [ ${#AZURE_EXISTS} -gt 1 ]; then
-  echo -e "$BACKEND\tAzure Remote Exists. Pushing to backend..."
-  git push https://$USERNAME:$PASSWORD@itra-backend-uat.scm.azurewebsites.net:443/itra-backend-uat.git HEAD:master
-else
-  echo -e "$BACKEND\tAzure Remote Does not exist. Adding remote URL and pushing to backend."
-  git push remote add azure https://$USERNAME:$PASSWORD@itra-backend-uat.scm.azurewebsites.net:443/itra-backend-uat.git
-  git push azure HEAD:master
-fi
+echo -e "$BACKEND\tPushing to backend..."
+git push https://$USERNAME:$PASSWORD@itra-backend-uat.scm.azurewebsites.net:443/itra-backend-uat.git HEAD:master
 
 sleep 1
 echo -e "$FROTEND\tPushing to Front End. Please ensure that the folder is on the same level as backend and NPM is installed"
