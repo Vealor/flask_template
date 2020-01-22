c3676ad960a70dd5c57a1c602fae155f1274016b
Will Thompson <willthompson@kpmg.ca>
2020-01-21 17:19:28 -0800

    Add tests for client update and delete endpoints

--- test/functional/test_clients.py
+++ test/functional/test_clients.py
@@ -1,6 +1,6 @@
 import pytest
 from src import db
-from src.models import Client
+from src.models import Client, Project
 from test._helpers import login, get_req, post_req, put_req, delete_req
 from test import api, client
 
@@ -248,7 +248,7 @@ class TestClientUpdate():
         helper_token = login(client, 'lh-admin', 'Kpmg1234%')
         response = put_req('/clients/' + str(new_client_id), client, {
             'name': "Updated Client Two",
-            'client_entities': []
+            'client_entities': [{'company_code': 'CODE', 'lob_sector': 'business_services_business_services', 'jurisdictions': ['ab']}]
         }, helper_token)
 
         assert response.status_code == 200
@@ -260,6 +260,20 @@ class TestClientUpdate():
         new_client = Client.find_by_id(new_client_id)
         db.session.delete(new_client)
         db.session.commit()
+
+    def test_client_update_does_not_exist(self, api, client):
+
+        # Test body
+        helper_token = login(client, 'lh-admin', 'Kpmg1234%')
+        response = put_req('/clients/' + str(1), client, {
+            'name': "Client One",
+            'client_entities': []
+        }, helper_token)
+
+        assert response.status_code == 404  # NotFoundError
+        data = response.get_json()
+        assert data['status'] == 'Error 404'
+        assert data['payload'] == []
 
     def test_client_update_name_already_exists(self, api, client):
 
@@ -290,6 +304,93 @@ class TestClientUpdate():
         db.session.delete(new_client2)
         db.session.commit()
 
+    def test_client_update_invalid_entity(self, api, client):
+
+        # Test set-up
+        bad_client = Client(name = "Bad Client")
+        db.session.add(bad_client)
+        db.session.commit()
+        bad_client_id = Client.query.filter_by(name = "Bad Client").first().id
+
+        # Test body
+        # code is too long
+        helper_token = login(client, 'lh-admin', 'Kpmg1234%')
+        response = put_req('/clients/' + str(bad_client_id), client, {
+            'name': "Bad Client",
+            'client_entities': [{'company_code': 'CODE_IS_TOO_LONG', 'lob_sector': 'business_services_business_services', 'jurisdictions': ['ab', 'bc']}]
+        }, helper_token)
+
+        assert response.status_code == 400  # InputError
+        data = response.get_json()
+        assert data['status'] == 'Error 400'
+        assert data['payload'] == []
+
+        # not recognized lob sector
+        response = put_req('/clients/' + str(bad_client_id), client, {
+            'name': "Bad Client",
+            'client_entities': [{'company_code': 'CODE', 'lob_sector': 'floral_industry_toilet_brushes', 'jurisdictions': ['on']}]
+        }, helper_token)
+
+        assert response.status_code == 400  # InputError
+        data = response.get_json()
+        assert data['status'] == 'Error 400'
+        assert data['payload'] == []
+
+        # no jurisdiction
+        response = put_req('/clients/' + str(bad_client_id), client, {
+            'name': "Bad Client",
+            'client_entities': [{'company_code': 'CODE', 'lob_sector': 'business_services_business_services', 'jurisdictions': []}]
+        }, helper_token)
+
+        assert response.status_code == 400  # InputError
+        data = response.get_json()
+        assert data['status'] == 'Error 400'
+        assert data['payload'] == []
+
+        # same company code
+        response = put_req('/clients/' + str(bad_client_id), client, {
+            'name': "Bad Client",
+            'client_entities': [
+                {'company_code': 'CODE', 'lob_sector': 'business_services_business_services', 'jurisdictions': ['bc']},
+                {'company_code': 'CODE', 'lob_sector': 'business_services_business_services', 'jurisdictions': ['ab']}
+            ]
+        }, helper_token)
+
+        assert response.status_code == 400  # InputError
+        data = response.get_json()
+        assert data['status'] == 'Error 400'
+        assert data['payload'] == []
+
+        # bad jurisdiction
+        response = put_req('/clients/' + str(bad_client_id), client, {
+            'name': "Bad Client",
+            'client_entities': [
+                {'company_code': 'CODE', 'lob_sector': 'business_services_business_services', 'jurisdictions': ['tx']}
+            ]
+        }, helper_token)
+
+        assert response.status_code == 400  # InputError
+        data = response.get_json()
+        assert data['status'] == 'Error 400'
+        assert data['payload'] == []
+
+        #duplicate jurisdiction
+        response = put_req('/clients/' + str(bad_client_id), client, {
+            'name': "Bad Client",
+            'client_entities': [
+                {'company_code': 'CODE', 'lob_sector': 'business_services_business_services', 'jurisdictions': ['ab', 'ab']}
+            ]
+        }, helper_token)
+
+        assert response.status_code == 400  # InputError
+        data = response.get_json()
+        assert data['status'] == 'Error 400'
+        assert data['payload'] == []
+
+        # Test cleanup
+        db.session.delete(bad_client)
+        db.session.commit()
+
 @pytest.mark.clients
 class TestClientDelete():
 
@@ -310,6 +411,41 @@ class TestClientDelete():
         assert data['status'] == 'ok'
         assert len(data['payload']) > 0
         assert Client.find_by_id(new_client_id) is None
+
+    def test_client_delete_fail_project_exists(self, api, client):
+
+        # Test set-up
+        new_client = Client(name = "Client C")
+        db.session.add(new_client)
+        db.session.commit()
+        new_client_id = Client.query.filter_by(name = "Client C").first().id
+
+        new_project = Project(name = "Project C", client_id = new_client_id, lead_partner_id = 2, lead_manager_id = 3)
+        db.session.add(new_project)
+        db.session.commit()
+
+        # Test body
+        helper_token = login(client, 'lh-admin', 'Kpmg1234%')
+        response = delete_req('/clients/' + str(new_client_id), client, helper_token)
+
+        assert response.status_code == 400
+        data = response.get_json()
+        assert data['status'] == 'Error 400'
+        assert len(data['payload']) == 0
+
+        db.session.delete(new_project)
+        db.session.delete(new_client)
+
+    def test_client_delete_fail_client_dne(self, api, client):
+
+        # Test body
+        helper_token = login(client, 'lh-admin', 'Kpmg1234%')
+        response = delete_req('/clients/' + str(23), client, helper_token)
+
+        assert response.status_code == 404
+        data = response.get_json()
+        assert data['status'] == 'Error 404'
+        assert len(data['payload']) == 0
 
 
 # @pytest.mark.clients
