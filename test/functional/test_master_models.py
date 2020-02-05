22ebce04d30913bf6d128e1aa8748b5c4bb61507
Will Thompson <willthompson@kpmg.ca>
2020-02-04 17:25:45 -0800

    Add delete test to master models

--- test/functional/test_master_models.py
+++ test/functional/test_master_models.py
@@ -130,7 +130,51 @@ class TestMasterModelsGet():
         db.session.commit()
         assert MasterModel.query.count() == 0
 
-    def test_master_model_set_active(self, api, client):
+# @pytest.mark.master_models
+# class TestMasterModelsUpdate():
+#
+#     def test_master_model_set_active(self, api, client):
+#
+#         # Test set-up
+#         model_data_dict = {
+#             'train_data_start': get_date_obj_from_str("2000-01-01"),
+#             'train_data_end': get_date_obj_from_str("2000-03-01"),
+#             'pickle': pickle.dumps(None),
+#             'hyper_p': {}
+#         }
+#
+#         mm1 = MasterModel(**model_data_dict)
+#         db.session.add(mm1)
+#         db.session.commit()
+#         mm1_id = mm1.id
+#         assert MasterModel.query.count() == 1
+#
+#         # Test body
+#         helper_token = login(client, 'lh-admin', 'Kpmg1234%')
+#         response = put_req('/master_models/' + str(mm1_id) + '/set_active/', client, {}, helper_token)
+#         print(response.get_json())
+#         assert response.status_code == 400
+#         data = response.get_json()
+#         assert data['status'] == 'Error 400'
+#
+#         mm1.status = 'pending'
+#         db.session.add(mm1)
+#         db.session.commit()
+#
+#         response2 = put_req('/master_models/' + str(mm1_id) + '/set_active/', client, {}, helper_token)
+#         assert response2.status_code == 200
+#         data = response2.get_json()
+#         assert data['status'] == 'ok'
+#
+#         # Test clean-up
+#         db.session.delete(mm1)
+#         db.session.commit()
+#         assert MasterModel.query.count() == 0
+
+@pytest.mark.master_models
+class TestMasterModelsDelete():
+
+    def test_master_model_delete(self, api, client):
 
         # Test set-up
         model_data_dict = {
@@ -148,22 +192,13 @@ class TestMasterModelsGet():
 
         # Test body
         helper_token = login(client, 'lh-admin', 'Kpmg1234%')
-        response = put_req('/master_models/' + str(mm1_id) + '/set_active/', client, {}, helper_token)
-        print(response.get_json())
-        assert response.status_code == 400
+        response = delete_req('/master_models/9000', client, helper_token)
+        assert response.status_code == 404
         data = response.get_json()
-        assert data['status'] == 'Error 400'
+        assert data['status'] == 'Error 404'
 
-        mm1.status = 'pending'
-        db.session.add(mm1)
-        db.session.commit()
-
-        response2 = put_req('/master_models/' + str(mm1_id) + '/set_active/', client, {}, helper_token)
+        response2 = delete_req('/master_models/' + str(mm1_id), client, helper_token)
         assert response2.status_code == 200
         data = response2.get_json()
         assert data['status'] == 'ok'
-
-        # Test clean-up
-        db.session.delete(mm1)
-        db.session.commit()
         assert MasterModel.query.count() == 0
