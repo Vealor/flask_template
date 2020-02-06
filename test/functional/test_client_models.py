3eddecec1cbaee08342d952e69c2b4af45e0da69
Will Thompson <willthompson@kpmg.ca>
2020-02-06 11:44:33 -0800

    Complete a first pass of client model tests

--- test/functional/test_client_models.py
+++ test/functional/test_client_models.py
@@ -9,11 +9,12 @@ from test import api, client
 @pytest.mark.client_models
 class TestClientModelsGet():
 
-    def test_master_model_get_one(self, api, client):
+    def test_client_model_get_one(self, api, client):
 
         # Test set-up
 
-        db.session.add(Client(name = "Client A"))
+        new_client = Client(name = "Client A")
+        db.session.add(new_client)
         db.session.commit()
         new_client_id = Client.query.filter_by(name = "Client A").first().id
 
@@ -43,164 +44,203 @@ class TestClientModelsGet():
         # Test clean-up
         db.session.delete(cm1)
         db.session.delete(cm2)
+        db.session.delete(new_client)
+        db.session.commit()
+        assert ClientModel.query.count() == 0
+        assert Client.query.count() == 0
+
+    def test_client_model_get_all(self, api, client):
+
+        # Test set-up
+        new_client = Client(name = "Client B")
+        db.session.add(new_client)
+        db.session.commit()
+        new_client_id = Client.query.filter_by(name = "Client B").first().id
+
+        model_data_dict = {
+            'client_id': new_client_id,
+            'train_data_start': get_date_obj_from_str("2000-01-01"),
+            'train_data_end': get_date_obj_from_str("2000-03-01"),
+            'pickle': pickle.dumps(None),
+            'hyper_p': {}
+        }
+
+        cm1 = ClientModel(**model_data_dict)
+        cm2 = ClientModel(**model_data_dict)
+        db.session.add(cm1)
+        db.session.add(cm2)
         db.session.commit()
 
-    # def test_master_model_get_all(self, api, client):
-    #
-    #     # Test set-up
-    #     model_data_dict = {
-    #         'train_data_start': get_date_obj_from_str("2000-01-01"),
-    #         'train_data_end': get_date_obj_from_str("2000-03-01"),
-    #         'pickle': pickle.dumps(None),
-    #         'hyper_p': {}
-    #     }
-    #
-    #     mm1 = MasterModel(**model_data_dict)
-    #     mm2 = MasterModel(**model_data_dict)
-    #     db.session.add(mm1)
-    #     db.session.add(mm2)
-    #     db.session.commit()
-    #
-    #     # Test body
-    #     helper_token = login(client, 'lh-admin', 'Kpmg1234%')
-    #     response = get_req('/master_models/', client, helper_token)
-    #     assert response.status_code == 200
-    #     data = response.get_json()
-    #     assert data['status'] == 'ok'
-    #     assert len(data['payload']) == 2
-    #
-    #     # Test clean-up
-    #     db.session.delete(mm1)
-    #     db.session.delete(mm2)
-    #     db.session.commit()
-    #     assert MasterModel.query.count() == 0
-    #
-    # def test_master_model_get_status_training(self, api, client):
-    #
-    #     # Test set-up
-    #     model_data_dict = {
-    #         'train_data_start': get_date_obj_from_str("2000-01-01"),
-    #         'train_data_end': get_date_obj_from_str("2000-03-01"),
-    #         'pickle': pickle.dumps(None),
-    #         'hyper_p': {}
-    #     }
-    #
-    #     mm1 = MasterModel(**model_data_dict)
-    #     db.session.add(mm1)
-    #     db.session.commit()
-    #     assert MasterModel.query.count() == 1
-    #
-    #     # Test body
-    #     helper_token = login(client, 'lh-admin', 'Kpmg1234%')
-    #     response = get_req('/master_models/is_training/', client, helper_token)
-    #     assert response.status_code == 200
-    #     data = response.get_json()
-    #     assert data['status'] == 'ok'
-    #     assert data['payload']['is_training'] is True
-    #
-    #     # Test clean-up
-    #     db.session.delete(mm1)
-    #     db.session.commit()
-    #     assert MasterModel.query.count() == 0
-    #
-    # def test_master_model_get_status_pending(self, api, client):
-    #
-    #     # Test set-up
-    #     model_data_dict = {
-    #         'train_data_start': get_date_obj_from_str("2000-01-01"),
-    #         'train_data_end': get_date_obj_from_str("2000-03-01"),
-    #         'pickle': pickle.dumps(None),
-    #         'hyper_p': {}
-    #     }
-    #
-    #     mm1 = MasterModel(**model_data_dict)
-    #     mm1.status = 'pending'
-    #     db.session.add(mm1)
-    #     db.session.commit()
-    #     assert MasterModel.query.count() == 1
-    #
-    #     # Test body
-    #     helper_token = login(client, 'lh-admin', 'Kpmg1234%')
-    #     response = get_req('/master_models/has_pending/', client, helper_token)
-    #     assert response.status_code == 200
-    #     data = response.get_json()
-    #     assert data['status'] == 'ok'
-    #     assert data['payload']['is_pending'] is True
-    #
-    #     # Test clean-up
-    #     db.session.delete(mm1)
-    #     db.session.commit()
-    #     assert MasterModel.query.count() == 0
+        # Test body
+        helper_token = login(client, 'lh-admin', 'Kpmg1234%')
+        response = get_req('/client_models/', client, helper_token)
+        assert response.status_code == 200
+        data = response.get_json()
+        assert data['status'] == 'ok'
+        assert len(data['payload']) == 2
 
-# @pytest.mark.master_models
-# class TestMasterModelsUpdate():
+        # Test clean-up
+        db.session.delete(cm1)
+        db.session.delete(cm2)
+        db.session.delete(new_client)
+        db.session.commit()
+        assert ClientModel.query.count() == 0
+        assert Client.query.count() == 0
+
+    def test_client_model_get_status_training(self, api, client):
+
+        # Test set-up
+        new_client = Client(name = "Client C")
+        db.session.add(new_client)
+        db.session.commit()
+        new_client_id = Client.query.filter_by(name = "Client C").first().id
+
+        model_data_dict = {
+            'client_id': new_client_id,
+            'train_data_start': get_date_obj_from_str("2000-01-01"),
+            'train_data_end': get_date_obj_from_str("2000-03-01"),
+            'pickle': pickle.dumps(None),
+            'hyper_p': {}
+        }
+
+        cm1 = ClientModel(**model_data_dict)
+        db.session.add(cm1)
+        db.session.commit()
+        assert ClientModel.query.count() == 1
+
+        # Test body
+        helper_token = login(client, 'lh-admin', 'Kpmg1234%')
+        response = get_req('/client_models/is_training?client_id={}'.format(new_client_id), client, helper_token)
+        assert response.status_code == 200
+        data = response.get_json()
+        assert data['status'] == 'ok'
+        assert data['payload']['is_training'] is True
+
+        # Test clean-up
+        db.session.delete(cm1)
+        db.session.delete(new_client)
+        db.session.commit()
+        assert ClientModel.query.count() == 0
+        assert Client.query.count() == 0
+
+    def test_client_model_get_status_pending(self, api, client):
+
+        # Test set-up
+        new_client = Client(name = "Client D")
+        db.session.add(new_client)
+        db.session.commit()
+        new_client_id = Client.query.filter_by(name = "Client D").first().id
+
+        model_data_dict = {
+            'client_id': new_client_id,
+            'train_data_start': get_date_obj_from_str("2000-01-01"),
+            'train_data_end': get_date_obj_from_str("2000-03-01"),
+            'pickle': pickle.dumps(None),
+            'hyper_p': {}
+        }
+
+        cm1 = ClientModel(**model_data_dict)
+        cm1.status = 'pending'
+        db.session.add(cm1)
+        db.session.commit()
+        assert ClientModel.query.count() == 1
+
+        # Test body
+        helper_token = login(client, 'lh-admin', 'Kpmg1234%')
+        response = get_req('/client_models/has_pending?client_id={}'.format(new_client_id), client, helper_token)
+        assert response.status_code == 200
+        data = response.get_json()
+        assert data['status'] == 'ok'
+        assert data['payload']['is_pending'] is True
+
+        # Test clean-up
+        db.session.delete(cm1)
+        db.session.delete(new_client)
+        db.session.commit()
+        assert ClientModel.query.count() == 0
+        assert Client.query.count() == 0
+
+# @pytest.mark.client_models
+# class TestClientModelsUpdate():
 #
-#     def test_master_model_set_active(self, api, client):
+#     def test_client_model_set_active(self, api, client):
 #
 #         # Test set-up
+#         new_client = Client(name = "Client E")
+#         db.session.add(new_client)
+#         db.session.commit()
+#         new_client_id = Client.query.filter_by(name = "Client E").first().id
+#
 #         model_data_dict = {
+#             'client_id': new_client_id,
 #             'train_data_start': get_date_obj_from_str("2000-01-01"),
 #             'train_data_end': get_date_obj_from_str("2000-03-01"),
 #             'pickle': pickle.dumps(None),
 #             'hyper_p': {}
 #         }
 #
-#         mm1 = MasterModel(**model_data_dict)
-#         db.session.add(mm1)
+#         cm1 = ClientModel(**model_data_dict)
+#         db.session.add(cm1)
 #         db.session.commit()
-#         mm1_id = mm1.id
-#         assert MasterModel.query.count() == 1
+#         cm1_id = cm1.id
+#         assert ClientModel.query.count() == 1
 #
 #         # Test body
 #         helper_token = login(client, 'lh-admin', 'Kpmg1234%')
-#         response = put_req('/master_models/' + str(mm1_id) + '/set_active/', client, {}, helper_token)
+#         response = put_req('/client_models/' + str(cm1_id) + '/set_active/', client, {}, helper_token)
 #         print(response.get_json())
 #         assert response.status_code == 400
 #         data = response.get_json()
 #         assert data['status'] == 'Error 400'
 #
-#         mm1.status = 'pending'
-#         db.session.add(mm1)
+#         cm1.status = 'pending'
+#         db.session.add(cm1)
 #         db.session.commit()
 #
-#         response2 = put_req('/master_models/' + str(mm1_id) + '/set_active/', client, {}, helper_token)
+#         response2 = put_req('/client_models/' + str(cm1_id) + '/set_active/', client, {}, helper_token)
 #         assert response2.status_code == 200
 #         data = response2.get_json()
 #         assert data['status'] == 'ok'
 #
 #         # Test clean-up
-#         db.session.delete(mm1)
+#         db.session.delete(cm1)
 #         db.session.commit()
-#         assert MasterModel.query.count() == 0
+#         assert ClientModel.query.count() == 0
 
-# @pytest.mark.master_models
-# class TestMasterModelsDelete():
-#
-#     def test_master_model_delete(self, api, client):
-#
-#         # Test set-up
-#         model_data_dict = {
-#             'train_data_start': get_date_obj_from_str("2000-01-01"),
-#             'train_data_end': get_date_obj_from_str("2000-03-01"),
-#             'pickle': pickle.dumps(None),
-#             'hyper_p': {}
-#         }
-#
-#         mm1 = MasterModel(**model_data_dict)
-#         db.session.add(mm1)
-#         db.session.commit()
-#         mm1_id = mm1.id
-#         assert MasterModel.query.count() == 1
-#
-#         # Test body
-#         helper_token = login(client, 'lh-admin', 'Kpmg1234%')
-#         response = delete_req('/master_models/9000', client, helper_token)
-#         assert response.status_code == 404
-#         data = response.get_json()
-#         assert data['status'] == 'Error 404'
-#
-#         response2 = delete_req('/master_models/' + str(mm1_id), client, helper_token)
-#         assert response2.status_code == 200
-#         data = response2.get_json()
-#         assert data['status'] == 'ok'
-#         assert MasterModel.query.count() == 0
+@pytest.mark.client_models
+class TestClientModelsDelete():
+
+    def test_client_model_delete(self, api, client):
+
+        # Test set-up
+        new_client = Client(name = "Client F")
+        db.session.add(new_client)
+        db.session.commit()
+        new_client_id = Client.query.filter_by(name = "Client F").first().id
+
+        model_data_dict = {
+            'client_id': new_client_id,
+            'train_data_start': get_date_obj_from_str("2000-01-01"),
+            'train_data_end': get_date_obj_from_str("2000-03-01"),
+            'pickle': pickle.dumps(None),
+            'hyper_p': {}
+        }
+
+        cm1 = ClientModel(**model_data_dict)
+        db.session.add(cm1)
+        db.session.commit()
+        cm1_id = cm1.id
+        assert ClientModel.query.count() == 1
+
+        # Test body
+        helper_token = login(client, 'lh-admin', 'Kpmg1234%')
+        response = delete_req('/client_models/9000', client, helper_token)
+        assert response.status_code == 404
+        data = response.get_json()
+        assert data['status'] == 'Error 404'
+
+        response2 = delete_req('/client_models/' + str(cm1_id), client, helper_token)
+        assert response2.status_code == 200
+        data = response2.get_json()
+        assert data['status'] == 'ok'
+        assert ClientModel.query.count() == 0
